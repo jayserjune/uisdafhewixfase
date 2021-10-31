50bb1d64ad41211b9aaec26a8500f4b37ce384e6
jayserjune <55340094+jayserjune@users.noreply.github.com>
2021-10-31 14:27:03 +0800

    Add files via upload

---
+++ config_new_7001/app.js
@@ -0,0 +1,11 @@
+
+            module.exports = {
+              port:7001,
+              account:'',
+              api_id : 1880415,
+              api_hash : 'be7ba475002441895695d3a23c703240',
+              expired_time:1635430385968,
+              level:0,
+              account_max:3
+            };
+            
